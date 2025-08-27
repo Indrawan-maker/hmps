2165c90ac257a801f06110b14ed61ebdb2580928
Indrawan <indrawanismail0@gmail.com>
2025-08-28 02:07:36 +0800

    feat: card

---
+++ src/app/components/Card.jsx
@@ -0,0 +1,15 @@
+import { LuGithub } from "react-icons/lu";
+
+export default function Card() {
+    return (
+        <main className="p-0 md:p-8 flex justify-center items-center">
+        <main className="w-120 h-120 bg-blue-200 rounded-3xl p-6">
+            <div className="w-full h-full">
+                <div>
+                    <div><LuGithub /></div>
+                </div>
+            </div>
+        </main>
+        </main>
+    )
+}
